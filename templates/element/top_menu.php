a4c0f80e1ad8c5900356baba44e5a2745bfdf55d
hk-okumura <okumura@human-ook.jp>
2025-07-31 18:55:02 +0900

    トップメニューの言語設定の修正

--- templates/element/top_menu.php
+++ templates/element/top_menu.php
@@ -4,33 +4,68 @@
  * @var \App\View\AppView $this
  * @var array $currentuser
  */
-
+$topMenus = [
+    [
+        'title'=>__('label_home'),
+        'url'=>['controller'=>'Welcome','action'=>'index'],
+        'class'=>'home',
+        'scope'=>'public',
+    ],
+    [
+        'title'=>__('label_my_page'),
+        'url'=>['controller'=>'My','action'=>'page'],
+        'class'=>'my-page',
+        'scope'=>'private',
+    ],
+    [
+        'title'=>__('label_project'),
+        'url'=>['controller'=>'Projects','action'=>'index'],
+        'class'=>'projects',
+        'scope'=>'private',
+    ],
+    [
+        'title'=>__('label_administration'),
+        'url'=>['controller'=>'Admin','action'=>'index'],
+        'class'=>'administration',
+        'scope'=>'admin',
+    ],
+    [
+        'title'=>__('label_help'),
+        'url'=>'https://www.redmine.org/guide',
+        'class'=>'help',
+        'scope'=>'external',
+    ],
+];
 ?>
 <div id="account">
     <?php if ($this->CandyUser->isLoggedIn()) : ?>
         <ul>
-            <li><?= $this->Html->link(__('My account'), ['controller'=>'My','action'=>'account'], ['class' => 'my-account']); ?></li>
-            <li><?= $this->Html->link(__('Sign out'), ['controller'=>'Account','action'=>'logout'], ['class' => 'logout']); ?></li>
+            <li><?= $this->Html->link(__('label_my_account'), ['controller'=>'My','action'=>'account'], ['class' => 'my-account']); ?></li>
+            <li><?= $this->Html->link(__('label_logout'), ['controller'=>'Account','action'=>'logout'], ['class' => 'logout']); ?></li>
         </ul>
     <?php else : ?>
         <ul>
-            <li><?= $this->Html->link(__('Sign in'), ['controller'=>'Account','action'=>'login'], ['class' => 'login']); ?></li>
-            <li><?= $this->Html->link(__('Register'), ['controller'=>'Account','action'=>'register'], ['class' => 'register']); ?></li>
+            <li><?= $this->Html->link(__('label_login'), ['controller'=>'Account','action'=>'login'], ['class' => 'login']); ?></li>
+            <li><?= $this->Html->link(__('label_register'), ['controller'=>'Account','action'=>'register'], ['class' => 'register']); ?></li>
         </ul>
     <?php endif; ?>
 </div>
 <?php if ($this->CandyUser->isLoggedIn()) : ?>
     <div id="loggedas">
-        <?= __('Logged in as') ?>
+        <?= __('label_logged_as') ?>
         <?= $this->CandyUser->getProfileLink() ?>
     </div>
 <?php endif; ?>
 <ul>
-    <?php if ($this->CandyUser->isLoggedIn()) : ?>
-        <?php foreach ($this->TopMenu->getTopMenu() as $item) : ?>
-            <li><?= $this->Html->link(__($item['caption']), $item['url'], ['class' => $item['class']]) ?></li>
-        <?php endforeach; ?>
-    <?php else : ?>
-        <li></li>
-    <?php endif; ?>
+    <?php foreach ($topMenus as $item) : ?>
+        <?php if ($item['scope'] === 'public' || ( $item['scope'] === 'private' && $this->CandyUser->isLoggedIn())) : ?>
+        <li><?= $this->Html->link(__($item['title']), $item['url'], ['class' => $item['class']]) ?></li>
+        <?php endif; ?>
+        <?php if ($item['scope'] === 'admin' && $this->CandyUser->isLoggedIn() ) : ?>
+            <li><?= $this->Html->link(__($item['title']), $item['url'], ['class' => $item['class']]) ?></li>
+        <?php endif; ?>
+        <?php if ($item['scope'] === 'external') : ?>
+            <li><?= $this->Html->link(__($item['title']), $item['url'], ['class' => $item['class'],'rel'=>'noopener','target'=>'_blank']) ?></li>
+        <?php endif; ?>
+    <?php endforeach; ?>
 </ul>
