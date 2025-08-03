e8b25e6f38d1d368702125e2f9044c31a2eec841
hk-okumura <okumura@human-ook.jp>
2025-08-03 13:08:54 +0900

    getFullName メソッドの追加

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -3,24 +3,45 @@ declare(strict_types=1);
 
 namespace App\Model\Entity;
 
-use Authentication\IdentityInterface;
+use Cake\Core\Configure;
 use Cake\ORM\Entity;
 
-class User extends Entity implements IdentityInterface
+class User extends Entity
 {
+
     /**
-     * Authentication\IdentityInterface method
+     * @return string
      */
-    public function getIdentifier(): string
+    public function getFullName(): string
     {
-        return $this->id;
+        $firstName = $this->get('firstname');
+        $lastName = $this->get('lastname');
+        $ret = Configure::read('CandyCaneSettings.user_format', 'firstname_lastname');
+        $ret = str_replace('firstname', $firstName, $ret);
+        $ret = str_replace('lastname', $lastName, $ret);
+        $ret = str_replace('comma', ',', $ret);
+        $ret = str_replace('firstinitial', substr($firstName, 0, 1), $ret);
+        $ret = str_replace('lastinitial', substr($lastName, 0, 1), $ret);
+        return $ret;
     }
 
     /**
-     * Authentication\IdentityInterface method
+     * @return string
      */
-    public function getOriginalData(): Entity
+    public function getLanguageCode(): string
     {
-        return $this;
+        $value = $this->get('language');
+        $ret = '';
+        switch ($value) {
+            case 'ja':
+                $ret = 'ja_JP';
+                break;
+            case 'en':
+                $ret = 'en_US';
+                break;
+            default:
+                break;
+        }
+        return $ret;
     }
 }
