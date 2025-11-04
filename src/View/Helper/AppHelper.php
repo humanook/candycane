05f5723ff6d0450bb4c09542df0be7b8b3e77159
hk-okumura <okumura@human-ook.jp>
2025-11-04 11:07:44 +0900

    ベースの Helper クラス

---
+++ src/View/Helper/AppHelper.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+
+/**
+ * Application Helper
+ *
+ * @package candycane
+ */
+class AppHelper extends Helper
+{
+    /**
+     * initialize
+     * @param array $config
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+    }
+
+}
